c8eb265cf42b020dec348cdc00571757bd37f5c5
Oskar Mendel <brokenprogrammer@gmail.com>
2021-01-05 15:58:35 +0100

    Removed bad compiler flag.

--- test/build_gcc.sh
+++ test/build_gcc.sh
@@ -1,3 +1,3 @@
 g++ ./stn_unittest.cpp -DBUILD_LINUX=1 -DBUILD_WINDOWS=0 -o ./stn_unittest
 g++ ./stn_memory_unittest.cpp -DBUILD_LINUX=1 -DBUILD_WINDOWS=0 -o ./stn_memory_unittest
-g++ ./stn_string_unittest.cpp -DBUILD_LINUX=1 -DBUILD_WINDOWS=0 -Wc++11-compat-deprecated-writable-strings -o ./stn_string_unittest
+g++ ./stn_string_unittest.cpp -DBUILD_LINUX=1 -DBUILD_WINDOWS=0 -o ./stn_string_unittest
